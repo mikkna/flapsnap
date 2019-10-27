9af721720f88cdb116181d69bfe0a12763625a09
mikk.narusk <mikk.narusk@transferwise.com>
2019-10-27 19:45:39 +0200

    Updates

---
+++ precache-manifest.6fb2e8ea8e8f91ca1f3dfe608642d086.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfcb1b96d710d167d38a328dcfda1dbf",
+    "url": "/flapsnap/index.html"
+  },
+  {
+    "revision": "d99fd7198c25c63087f0",
+    "url": "/flapsnap/static/css/main.6b7824d5.chunk.css"
+  },
+  {
+    "revision": "bcdd2df691f9b1766482",
+    "url": "/flapsnap/static/js/2.f3e8b822.chunk.js"
+  },
+  {
+    "revision": "d99fd7198c25c63087f0",
+    "url": "/flapsnap/static/js/main.3a871792.chunk.js"
+  },
+  {
+    "revision": "1a2cf2cdd991be18eb82",
+    "url": "/flapsnap/static/js/runtime-main.56a2cae4.js"
+  }
+]);
